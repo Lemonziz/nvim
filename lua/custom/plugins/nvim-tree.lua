de201910cf5d968c556af27dad6cb30f683d3878
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2024-09-22 13:08:13 +0800

    remove key binding for <C-k> in explore mode which affect tmux navigator

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -3,12 +3,23 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		local nvimtree = require("nvim-tree")
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			api.config.mappings.default_on_attach(bufnr)
+			-- remove a default
+			vim.keymap.del("n", "<C-k>", { buffer = bufnr })
+		end
 
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 
 		nvimtree.setup({
+			on_attach = my_on_attach,
 			view = {
 				width = 35,
 				relativenumber = true,
