93da25de96f017e6ec2f7dcf6dc3a882d04ece98
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2024-10-31 00:28:46 +0800

    vimtex setting

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+vim.keymap.set("n", "<leader>ww", "<cmd>set wrap<CR>", { desc = "Toggle wrap" })
+vim.keymap.set("n", "<leader>nw", "<cmd>set nowrap<CR>", { desc = "Toggle nowrap" })
 
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<cr>", { desc = "Open new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { desc = "Close current tab" })
---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,12 @@
+return {
+	"lervag/vimtex",
+	lazy = false, -- we don't want to lazy load VimTeX
+	-- tag = "v2.15", -- uncomment to pin to a specific release
+	init = function()
+		vim.g.vimtex_view_method = "skim" -- Built-in viewer method
+		vim.g.vimtex_view_general_viewer = "skim"
+		vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+		vim.g.vimtex_compiler_method = "latexmk"
+		vim.g.vimtex_quickfix_open_on_warning = 0
+	end,
+}
