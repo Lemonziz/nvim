c98afc828d418e83654770825ef60ba5a2e07304
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2025-05-25 19:52:24 +0800

    VimEnter -> VeryLazy

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: missing-fields
 return {
 	"hrsh7th/nvim-cmp",
-	event = "VimEnter",
+	event = "VeryLazy",
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
