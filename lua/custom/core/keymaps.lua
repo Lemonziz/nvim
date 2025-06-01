0f88955d94ac0948afc72eb450375a35174fa727
Lemonziz <167009272+Lemonziz@users.noreply.github.com>
2025-06-01 16:45:08 +0800

    feat: keymaps for wrap

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -5,8 +5,7 @@ vim.keymap.set("n", "<leader>|", "<C-w>v", { desc = "Split window horizontally" 
 vim.keymap.set("n", "<leader>-", "<C-w>s", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-vim.keymap.set("n", "<leader>ww", "<cmd>set wrap<CR>", { desc = "Toggle wrap" })
-vim.keymap.set("n", "<leader>nw", "<cmd>set nowrap<CR>", { desc = "Toggle nowrap" })
+vim.keymap.set("n", "<leader>w", "<cmd>set wrap!<CR>", { desc = "Toggle wrap" })
 
 vim.keymap.set("n", "<leader>tc", "<cmd>tabnew<cr>", { desc = "Open new tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { desc = "Close current tab" })
